b39af303cfbc85893481e90c6669e7785abf559e
Jordi Borràs i Vivó <jordibv@gmail.com>
2021-11-15 23:51:35 +0100

    Nivell 3
    Upload del nivell 3

---
+++ ITAcademy/sprint1/s1_01e/Derivada.java
@@ -0,0 +1,10 @@
+package s1_01e;
+
+public class Derivada extends Base {
+
+	@Override
+	public void metode2() {
+		System.out.println("Mètode 2 a la classe Derivada");
+	}
+	
+}
---
+++ ITAcademy/sprint1/s1_01e/Main.java
@@ -0,0 +1,12 @@
+package s1_01e;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Base b = new Derivada();
+		
+		b.metode1();
+		
+	}
+
+}
