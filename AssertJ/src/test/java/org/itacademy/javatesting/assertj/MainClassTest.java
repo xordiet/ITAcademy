e43cff9e147aef9af7c5fed73df4801646201c7c
Jordi Borràs i Vivó <jordibv@gmail.com>
2021-11-26 11:52:40 +0100

    error a assertj

--- AssertJ/src/test/java/org/itacademy/javatesting/assertj/MainClassTest.java
+++ AssertJ/src/test/java/org/itacademy/javatesting/assertj/MainClassTest.java
@@ -1,4 +1,14 @@
 package org.itacademy.javatesting.assertj;
 
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
 public class MainClassTest {
+	@Test
+	  void a_few_simple_assertions() {
+	    assertThat("The Lord of the Rings").isNotNull()   
+	                                       .startsWith("The") 
+	                                       .contains("Lord") 
+	                                       .endsWith("Rings"); 
+	  }
 }
