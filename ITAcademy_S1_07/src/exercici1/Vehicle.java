4ca1e616eca9c3da2fd3f9cc906923b0e27f3f04
Jordi Borràs i Vivó <jordibv@gmail.com>
2021-12-09 11:46:19 +0100

    nivell 1

---
+++ ITAcademy_S1_07/src/exercici1/Vehicle.java
@@ -0,0 +1,30 @@
+package exercici1;
+
+public class Vehicle {
+	private String marca;
+	private String model;
+	
+	public void arrencar() {
+		System.out.println("brrroum!");
+	}
+	
+	public String claxon() {
+		return null;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+
+}
---
+++ ITAcademy_S1_07/src/exercici2/Moto.java
@@ -0,0 +1,25 @@
+package exercici2;
+
+public class Moto extends Vehicle {
+
+	@Override
+	public void arrencar() {
+		super.arrencar();
+	}
+
+	@Override
+	public String claxon() {
+		return "meec, meec!";
+	}
+	
+	/**
+	 * @deprecated
+	 * Aquest mètode està deprecated perquè ara hi ha motos elèctriques
+	 * que no tenen la funcionalitat de donar gas...
+	 */
+	@Deprecated
+	public void donarGas() {
+		System.out.println("rrrrrr!");
+	}
+	
+}
