42ca089f285bb0c2ab967599995ede741e84327d
Jordi Borràs i Vivó <jordibv@gmail.com>
2021-12-10 09:49:32 +0100

    nivell 3

---
+++ ITAcademy_S1_07/src/nivell3/MevaAnotacio.java
@@ -0,0 +1,10 @@
+package nivell3;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MevaAnotacio {
+	String directori() default "./";
+
+}
