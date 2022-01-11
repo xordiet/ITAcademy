d4e047499ec7f221e8bfa83715b696d1e723589f
Jordi Borràs i Vivó <jordibv@gmail.com>
2022-01-11 14:17:59 +0100

    reordenació

--- ITAcademy/pdf_s1_01/intro00/Producte.java
+++
@@ -1,9 +0,0 @@
-package intro00;
-
-public interface Producte {
-	public int numProductes = 5;
-	
-	public float getPreu();
-	public String getDescripcio();
-	
-}
--- ITAcademy/sprint1/s1_01c/Tricycle.java
+++
@@ -1,7 +0,0 @@
-package s1_01c;
-
-public class Tricycle extends Cycle{
-	public Tricycle() {
-		super.setRodes(3);
-	}
-}
